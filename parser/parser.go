27e2cbff9ddf25631b1d678b9179b1f5601f9e0f
saraikium <adnank122@gmail.com>
2023-12-26 00:23:41 +0500

    Feat(Parser): Add basic structure of the parser

---
+++ parser/parser.go
@@ -0,0 +1,29 @@
+package parser
+
+import (
+	"github.com/saraikium/monkey/ast"
+	"github.com/saraikium/monkey/lexer"
+	"github.com/saraikium/monkey/token"
+)
+
+type Parser struct {
+	l         *lexer.Lexer
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+	p.NextToken()
+	p.NextToken()
+	return p
+}
+
+func (p *Parser) NextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
